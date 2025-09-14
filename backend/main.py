703ad9fa0d6d8ddd937f1b7572c508cd6d6bd0f3
waffle-exe <127439240+waffle-exe@users.noreply.github.com>
2025-09-14 06:38:32 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -272,4 +272,6 @@ def get_all_users():
 
 # --- Main Entry Point ---
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
+
